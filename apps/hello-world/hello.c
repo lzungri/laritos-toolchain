558ecde7597e75034f7d816c6e0ff360ab134a9f
Leandro Zungri <lzungri@gmail.com>
2019-10-16 01:54:22 -0700

    app: Add function call to debug relocations

--- apps/hello-world/hello.c
+++ apps/hello-world/hello.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+void func(void) {
+    volatile char *p = 0;
+    *p = 'E';
+}
+
 int main(void) {
 //    puts("Hello World!");
     volatile char *p = (char *) 0xffff;
     *p = 1;
+    func();
     return 0;
 }
