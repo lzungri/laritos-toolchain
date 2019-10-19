74c6dacae069b4f9483cfbef627deba17dae5df3
Leandro Zungri <lzungri@gmail.com>
2019-10-18 17:29:52 -0700

    hello: Do not write garbage anywhere

--- apps/hello-world/hello.c
+++ apps/hello-world/hello.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
-void func(void) {
-    volatile char *p = 0;
-    *p = 'E';
-}
-
 char data[0xaa];
 char datac[16] = { 1, 2, 3 };
 char datac2[16] = { 4, 5, 6 };
 
+void func(void) {
+    data[1] = 0xaa;
+}
+
 int main(void) {
 //    puts("Hello World!");
-    volatile char *p = (char *) 0xffff;
-    *p = 1;
     func();
     data[0] = 0;
     datac[1] = 1;
