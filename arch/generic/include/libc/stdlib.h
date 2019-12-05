c5849c426c235ede407bd35860434990356f8c80
Leandro Zungri <lzungri@gmail.com>
2019-12-05 00:57:26 -0800

    syscall: Implement set_priority() syscall wrapper

--- arch/generic/include/libc/stdlib.h
+++ arch/generic/include/libc/stdlib.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <syscall.h>
+#include <stdint.h>
 
 DEF_SYSCALL(SYSCALL_EXIT, exit, int, status);
 DEF_SYSCALL(SYSCALL_YIELD, yield);
 DEF_SYSCALL(SYSCALL_GETPID, getpid);
+DEF_SYSCALL(SYSCALL_SET_PRIORITY, set_priority, uint8_t, priority);
 
 
