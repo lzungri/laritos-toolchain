dbd81264502c6a2f7dce5d8596171966ed257df7
Leandro Zungri <lzungri@gmail.com>
2019-12-06 19:01:43 -0800

    syscall: Implement set_process_name() syscall wrapper

--- arch/generic/include/libc/stdlib.h
+++ arch/generic/include/libc/stdlib.h
@@ -7,5 +7,6 @@ DEF_SYSCALL(SYSCALL_EXIT, exit, int, status);
 DEF_SYSCALL(SYSCALL_YIELD, yield);
 DEF_SYSCALL(SYSCALL_GETPID, getpid);
 DEF_SYSCALL(SYSCALL_SET_PRIORITY, set_priority, uint8_t, priority);
+DEF_SYSCALL(SYSCALL_SET_PROCESS_NAME, set_process_name, char *, name);
 
 
