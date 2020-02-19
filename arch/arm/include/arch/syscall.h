b2a692e0423a50fd597008cb2a83c8a95c7b8ef4
Leandro Zungri <lzungri@gmail.com>
2020-02-19 01:09:58 -0800

    arm: Add support for 2-args syscall

--- arch/arm/include/arch/syscall.h
+++ arch/arm/include/arch/syscall.h
@@ -20,3 +20,15 @@
         : "r" (a), "i" (no) \
         : "cc" /* Tell gcc this code modifies the cpsr */); \
 } while(0)
+
+#define ARCH_SYSCALL2(no, ret, t, a, t2, a2) do { \
+    /* volatile to prevent any gcc optimization on the assembly code */ \
+    asm volatile ( \
+        "mov r0, %1  \n" \
+        "mov r1, %2  \n" \
+        "svc %3      \n" \
+        "mov %0, r0  \n" \
+        : "=r" (ret) \
+        : "r" (a), "r" (a2), "i" (no) \
+        : "cc" /* Tell gcc this code modifies the cpsr */); \
+} while(0)
