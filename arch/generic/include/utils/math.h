e317b6e2bfb1a6aeaacbba066d12333842deb2f5
Leandro Zungri <lzungri@gmail.com>
2020-02-19 01:08:54 -0800

    math: Add math header

---
+++ arch/generic/include/utils/math.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#define min(a,b) \
+    ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+#define max(a,b) \
+    ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+#define abs(_n) ((_n) < 0 ? -(_n) : (_n))
+#define sign(_n) ((_n) < 0 ? -1 : 1)
+#define sign_extend_32(_n, _bits) (int32_t) \
+    (((_n) & (1 << ((_bits) - 1))) ? ((int32_t) (_n) - (1 << (_bits))) : (_n))
+
+
+#define nearly_equal(a, b, epsilon) \
+    ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a >= _b - epsilon && _a <= _b + epsilon;})
