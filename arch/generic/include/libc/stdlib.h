25ea51b0a407d503bc68acf0e3a85f8909a714d5
Leandro Zungri <lzungri@gmail.com>
2020-02-19 18:16:41 -0800

    syscall: Add backdoor syscall

--- arch/generic/include/libc/stdlib.h
+++ arch/generic/include/libc/stdlib.h
@@ -9,4 +9,10 @@ DEF_SYSCALL(SYSCALL_GETPID, getpid);
 DEF_SYSCALL(SYSCALL_SET_PRIORITY, set_priority, uint8_t, priority);
 DEF_SYSCALL(SYSCALL_SET_PROCESS_NAME, set_process_name, char *, name);
 
+/**
+ * This is just for debugging purposes. A production kernel will not have
+ * this syscall enabled, hopefully.
+ */
+DEF_SYSCALL(SYSCALL_BACKDOOR, backdoor, char *, cmd, void *, param);
+
 
