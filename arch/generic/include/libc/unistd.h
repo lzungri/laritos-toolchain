7b62a571fa9033db38f8459cff51a60e9757cbc3
Leandro Zungri <lzungri@gmail.com>
2020-02-21 00:00:13 -0800

    syscall: Add listdir() syscall wrapper

--- arch/generic/include/libc/unistd.h
+++ arch/generic/include/libc/unistd.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include <stdint.h>
 #include <syscall.h>
+#include <stdbool.h>
+
+#define CONFIG_FS_MAX_FILENAME_LEN 255
+
+typedef struct {
+    char name[CONFIG_FS_MAX_FILENAME_LEN];
+    bool isdir;
+} listdir_t;
 
 DEF_SYSCALL(SYSCALL_SLEEP, sleep, unsigned int, secs);
 DEF_SYSCALL(SYSCALL_MSLEEP, msleep, unsigned int, msecs);
 DEF_SYSCALL(SYSCALL_USLEEP, usleep, unsigned int, usecs);
 DEF_SYSCALL(SYSCALL_CHDIR, chdir, const char *, path);
 DEF_SYSCALL(SYSCALL_GETCWD, getcwd, char *, buf, int, buflen);
+DEF_SYSCALL(SYSCALL_LISTDIR, listdir, char *, path, uint32_t, offset, listdir_t *, dirs, int, dirslen);
