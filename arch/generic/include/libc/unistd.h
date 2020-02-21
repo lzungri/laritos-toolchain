afe657a442e9223dbb6000c188bb91c494fc125e
Leandro Zungri <lzungri@gmail.com>
2020-02-20 17:47:03 -0800

    syscall: Add chdir() and getcwd() syscall wrappers

--- arch/generic/include/libc/unistd.h
+++ arch/generic/include/libc/unistd.h
@@ -5,3 +5,5 @@
 DEF_SYSCALL(SYSCALL_SLEEP, sleep, unsigned int, secs);
 DEF_SYSCALL(SYSCALL_MSLEEP, msleep, unsigned int, msecs);
 DEF_SYSCALL(SYSCALL_USLEEP, usleep, unsigned int, usecs);
+DEF_SYSCALL(SYSCALL_CHDIR, chdir, const char *, path);
+DEF_SYSCALL(SYSCALL_GETCWD, getcwd, char *, buf, int, buflen);
