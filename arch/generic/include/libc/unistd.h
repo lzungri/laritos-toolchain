36de7f4c4f09b2f70074bdb40dba8c9433f22914
Leandro Zungri <lzungri@gmail.com>
2020-02-21 12:59:42 -0800

    syscall: Add open/read/write/close syscall wrappers

--- arch/generic/include/libc/unistd.h
+++ arch/generic/include/libc/unistd.h
@@ -11,9 +11,22 @@ typedef struct {
     bool isdir;
 } listdir_t;
 
+typedef enum {
+    ACCESS_MODE_EXEC = 1,
+    ACCESS_MODE_WRITE = 2,
+    ACCESS_MODE_READ = 4,
+    ACCESS_MODE_DIR = 8,
+} access_mode_t;
+
+typedef void file_t;
+
 DEF_SYSCALL(SYSCALL_SLEEP, sleep, unsigned int, secs);
 DEF_SYSCALL(SYSCALL_MSLEEP, msleep, unsigned int, msecs);
 DEF_SYSCALL(SYSCALL_USLEEP, usleep, unsigned int, usecs);
 DEF_SYSCALL(SYSCALL_CHDIR, chdir, const char *, path);
 DEF_SYSCALL(SYSCALL_GETCWD, getcwd, char *, buf, int, buflen);
 DEF_SYSCALL(SYSCALL_LISTDIR, listdir, char *, path, uint32_t, offset, listdir_t *, dirs, int, dirslen);
+DEF_SYSCALL_RET_TYPE(file_t *, SYSCALL_OPEN, open, char *, path, access_mode_t, mode);
+DEF_SYSCALL(SYSCALL_CLOSE, close, file_t *, file);
+DEF_SYSCALL(SYSCALL_READ, read, file_t *, file, void *, buf, int, buflen);
+DEF_SYSCALL(SYSCALL_WRITE, write, file_t *, file, void *, buf, int, buflen);
